a0b89a905128f287310adc6b1e4f3e2e34dfc411
Dylan <dylan@vandenbosch.co.za>
2018-11-13 15:13:21 +0200

    got .env with endpoint working

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -1,10 +1,12 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 var prisma_lib_1 = require("prisma-client-lib");
 var typeDefs = require("./prisma-schema").typeDefs;
 
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
-  endpoint: `https://eu1.prisma.sh/dylan-van-den-bosch-905763/market-mothership/dev`
+  endpoint: process.env.PRISMA_ENDPOINT
 });
 exports.prisma = new exports.Prisma();
