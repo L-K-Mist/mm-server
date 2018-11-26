7ea7c9c9552ac01a205141950fa34e663899d73e
Dylan <dylan@vandenbosch.co.za>
2018-11-26 12:16:08 +0200

    minor changes

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -3,35 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var prisma_lib_1 = require("prisma-client-lib");
 var typeDefs = require("./prisma-schema").typeDefs;
 
-var models = [
-  {
-    name: "Market",
-    embedded: false
-  },
-  {
-    name: "Post",
-    embedded: false
-  },
-  {
-    name: "Product",
-    embedded: false
-  },
-  {
-    name: "Province",
-    embedded: false
-  },
-  {
-    name: "Stall",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  }
-];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
-  models,
   endpoint: `${process.env["PRISMA_ENDPOINT"]}`,
   secret: `${process.env["PRISMA_SECRET"]}`
 });
