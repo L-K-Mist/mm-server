6b9facb0a5016d3825a8849a8122f52ce60a5b77
Dylan <dylan@vandenbosch.co.za>
2018-11-30 17:35:52 +0200

    before branching for more complete fetchMe

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,71 +1,82 @@
-const {
-  getUserId
-} = require('../utils')
+const { getUserId } = require("../utils");
 // const {
 //   prisma
 // } = require('../generated/prisma-client')
 
 const Query = {
-  me: (parent, args, ctx, info) => {
+  me: (parent, args, ctx) => {
     return ctx.db.user({
       id: getUserId(ctx)
-    })
+    });
   },
-  myStall: (parent, args, ctx) => {
-    return ctx.db.user({
-      id: getUserId(ctx)
-    }).stall()
+  myStall: (parent, args, ctx, info) => {
+    return ctx.binding.query.stalls(
+      {
+        where: {
+          owner: {
+            id: getUserId(ctx)
+          }
+        }
+      },
+      info
+    );
   },
-  stallProducts: (parent, {
-    stallId
-  }, ctx) => {
+  stallProducts: (parent, { stallId }, ctx) => {
     return ctx.db.products({
       where: {
         stall: {
           id: stallId
         }
       }
-    })
+    });
   },
   stalls: (parent, args, ctx, info) => {
-    return ctx.binding.query.stalls({
-      first: 20
-    }, info)
+    return ctx.binding.query.stalls(
+      {
+        first: 20
+      },
+      info
+    );
   },
 
   stall: (parent, args, ctx, info) => {
-    return ctx.binding.query.stall({
-      where: {
-        id: args.stallId
-      }
-    }, info)
+    return ctx.binding.query.stall(
+      {
+        where: {
+          id: args.stallId
+        }
+      },
+      info
+    );
   },
-
 
   myMarkets: (parent, args, ctx) => {
-    return ctx.db.user({
-      id: getUserId(ctx)
-    }).stall().markets()
+    return ctx.db
+      .user({
+        id: getUserId(ctx)
+      })
+      .stall()
+      .markets();
   },
-  feed: (parent, args, ctx) => ctx.db.posts({
-    where: {
-      isPublished: true
-    }
-  }),
+  feed: (parent, args, ctx) =>
+    ctx.db.posts({
+      where: {
+        isPublished: true
+      }
+    }),
   drafts: (parent, args, ctx) =>
     ctx.db.posts({
       where: {
         isPublished: false
       }
     }),
-  post: (parent, {
-    id
-  }, ctx) => ctx.db.post({
-    id
-  }),
+  post: (parent, { id }, ctx) =>
+    ctx.db.post({
+      id
+    }),
   markets: (parent, args, ctx) => ctx.db.markets()
-}
+};
 
 module.exports = {
-  Query,
-}
+  Query
+};
